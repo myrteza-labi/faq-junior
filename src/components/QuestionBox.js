1856974b38416bf5fd898ac57ff2ad0e22a0b854
Myrteza <myrteza@hotmail.fr>
2021-10-07 09:42:50 +0200

    :try: find the beginin of the code to toggle the answers

--- src/components/QuestionBox.js
+++ src/components/QuestionBox.js
@@ -1,13 +1,53 @@
 import React from 'react'; 
 import './QuestionBox.css'; 
 
+
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const QuestionBoxes = document.querySelectorAll(".QuestionBox");
+    QuestionBoxes.forEach((QuestionBox) => QuestionBox.addEventListener("click", () =>{
+  
+        if(QuestionBox.classList.contains("active")){
+            QuestionBox.classList.toggle("active")
+        }
+    else{
+        QuestionBoxes.forEach(QuestionBox => QuestionBox.classList.remove("active"))
+        QuestionBox.classList.add("active")
+        }
+
+    }))
+}) 
+
+
+
+
+
 function QuestionBox(props) {
     return(
-        <div className="QuestionBox" >
+        <div onClick={props.handleClickHanswer} className="QuestionBox" >
             <p className="question" >{props.question}</p>
-            <p className="answer">{props.answer}</p>
+            <p className={props.answerClassName}>{props.answer}</p>
         </div>
     )
 }
 
 export default QuestionBox; 
+/*
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const QuestionBoxes = document.querySelectorAll(".QuestionBox");
+    QuestionBoxes.forEach((QuestionBox) => QuestionBox.addEventListener("click", () =>{
+  
+        if(QuestionBox.classList.contains("active")){
+            QuestionBox.classList.toggle("active")
+        }
+    else{
+        QuestionBoxes.forEach(QuestionBox => QuestionBox.classList.remove("active"))
+        QuestionBox.classList.add("active")
+        }
+
+    }))
+}) 
+
+*/ 
